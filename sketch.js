21173d142f4da7c61f1b3f501feefb2ec4ab871b
Ace Figueroa <ace.g.fig@gmail.com>
2021-03-25 19:34:39 -0700

    added animations

--- sketch.js
+++ sketch.js
@@ -37,6 +37,7 @@ class sprite extends gameObject {
 //**********************************//
 let tilesX = 16; // 16
 let tilesY = 12; // 12
+var dinoFrame = 0;
 
 /* Defining the canvas and game state */
 let gameBoard = {
@@ -211,7 +212,10 @@ function setup() {
 }
 
 function draw() {
-  frameRate(4);
+  frameRate(8);
+  if(dinoFrame > 6){
+    dinoFrame = 0;
+  }
 
   // Collisions and movement
   playerMovement();
@@ -225,7 +229,38 @@ function draw() {
   // This is player
   imageMode(CENTER);
   playerRotato();
-  image(tileset, 0, 0, gameSprites["dinoF1F3"].width, gameSprites["dinoF1F3"].height,
-  gameSprites["dinoF1F3"].sx, gameSprites["dinoF1F3"].sy, gameSprites["dinoF1F3"].sWidth, 
-  gameSprites["dinoF1F3"].sHeight);
+  switch(Math.floor(dinoFrame/2)){
+
+    //Frame 1
+    case 0:
+      image(tileset, 0, 0, gameSprites["dinoF1F3"].width, gameSprites["dinoF1F3"].height,
+      gameSprites["dinoF1F3"].sx, gameSprites["dinoF1F3"].sy, gameSprites["dinoF1F3"].sWidth, 
+      gameSprites["dinoF1F3"].sHeight);
+    break;
+
+    //Frame 2
+    case 1:
+      image(tileset, 0, 0, gameSprites["dinoF2"].width, gameSprites["dinoF2"].height,
+      gameSprites["dinoF2"].sx, gameSprites["dinoF2"].sy, gameSprites["dinoF2"].sWidth, 
+      gameSprites["dinoF2"].sHeight);
+    break;
+
+    //Frame 3
+    case 2:
+      image(tileset, 0, 0, gameSprites["dinoF1F3"].width, gameSprites["dinoF1F3"].height,
+      gameSprites["dinoF1F3"].sx, gameSprites["dinoF1F3"].sy, gameSprites["dinoF1F3"].sWidth, 
+      gameSprites["dinoF1F3"].sHeight);
+    break;
+
+    //Frame 4
+    case 3:
+      image(tileset, 0, 0, gameSprites["dinoF4"].width, gameSprites["dinoF4"].height,
+      gameSprites["dinoF4"].sx, gameSprites["dinoF4"].sy, gameSprites["dinoF4"].sWidth, 
+      gameSprites["dinoF4"].sHeight);
+    break;
+  }
+
+  if(!gameBoard.paused){
+    dinoFrame++;
+  }
 }
